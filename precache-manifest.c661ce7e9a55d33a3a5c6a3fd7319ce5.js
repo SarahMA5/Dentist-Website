fc6bd38c6dc683412800fb66a5c378ac473d6da9
Sarah <alahmadisr@gmail.com>
2020-01-08 11:26:01 +0300

    Updates

---
+++ precache-manifest.c661ce7e9a55d33a3a5c6a3fd7319ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18655478763d40502664d30d686b1193",
+    "url": "/Dentist-Website/index.html"
+  },
+  {
+    "revision": "ebacb0ab3aa21b65d567",
+    "url": "/Dentist-Website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "685936f28bbe5c16ab41",
+    "url": "/Dentist-Website/static/js/2.7ce16401.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Dentist-Website/static/js/2.7ce16401.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebacb0ab3aa21b65d567",
+    "url": "/Dentist-Website/static/js/main.474c89f9.chunk.js"
+  },
+  {
+    "revision": "664b438ef6ef32e8c6e8",
+    "url": "/Dentist-Website/static/js/runtime-main.60bb8907.js"
+  }
+]);
